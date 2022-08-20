7c003a5af4e1815a76cf1b0bac9bdbd120e5df1c
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2022-08-20 16:05:55 -0300

    add config to run in huroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ChatGateway } from './chat.gateway';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({isGlobal: true,})],
   controllers: [],
   providers: [ChatGateway],
 })
