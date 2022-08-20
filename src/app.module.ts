e8911a00b1dd5f0486609d094ad9aea6f7bc5270
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2022-08-20 16:13:13 -0300

    refactor: remove code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { ChatGateway } from './chat.gateway';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true,})],
   controllers: [],
   providers: [ChatGateway],
 })
