91270e326351f61d239ddafe693ba98e047a168f
Gatão <gabrielgianotti5@gmail.com>
2023-02-04 22:49:27 -0300

    get lat, lng and satellites

--- main.js
+++ main.js
@@ -1,40 +1,50 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 const app = require('express')();
-require('dotenv').config();
-const port = process.env.PORT ?? 3000;
+require('dotenv').config()
+const port =process.env.PORT ?? 3000;
 var server = http.createServer(app);
 
 server.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+    console.log(`http://localhost:${port}`);
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+    res.sendFile(__dirname + '/index.html');
+  });
 
 wsServer = new WebSocketServer({
   httpServer: server
 });
 
 wsServer.on('request', (request) => {
-  let state = false;
+    let state = {};
 
-  let client = request.accept(null, request.origin);
+    let client = request.accept(null, request.origin);
 
-  client.on('message', (message) => {
-    if (message.type === 'utf8') {
-      console.log("oi", message.utf8Data);
-      client.sendUTF(state ? "ON" : "OFF");
-      client.sendUTF(message.utf8Data);
-    }
-  });
-  let interval = () => {
-    client.sendUTF(state ? "ON" : "OFF");
-    state = !state;
-  };
-  client.on('close', () => {
-    console.log("Conexão fechada");
-    clearInterval(interval);
-  });
+    client.on('message', (message) => {
+        if (message.type === 'utf8') {
+        
+          
+            const str =message.utf8Data;
+            const parts = str.split(',');
+            const sat = parts[0].split(':')[1];
+            const lat = parts[1].split(':')[1];
+            const lng = parts[2].split(':')[1];
+            state = { sat, lat, lng }
+            client.sendUTF("state");
+        }
+    });
+    client.on(' statusDrone', (message) => {
+        
+        client.sendUTF(state);
+        
+    });
+    let interval =() => {
+        client.sendUTF(state);
+    };
+    client.on('close', () => {
+        console.log("Conexão fechada");
+        clearInterval(interval);
+    });
 });
